9b76dfd556b0d7d8fad50f5e4af76245e28ad6b6
Budaházi Bence <budahazi.bence@petrik.hu>
2020-10-09 17:30:15 +0200

    Az alkalmazás működésének megvalósítása - felugró ablakokkal értesítés, új játék

--- app/src/main/java/com/example/thinkanumber/MainActivity.java
+++ app/src/main/java/com/example/thinkanumber/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.thinkanumber;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textTippeltSzam;
     private Button btnNovel, btnCsokkent, btnTippel;
     private int gondoltSzam, tippeltSzam, elet;
+    private AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +25,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+
+        ujJatek();
+
         btnTippel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (gondoltSzam < tippeltSzam){
-                    //TODO: értesítés, hogy a gondolt szám kisebb
+                    Toast.makeText(MainActivity.this, "A gondolt szám kisebb", Toast.LENGTH_SHORT).show();
                     eletLevon();
                 } else if (gondoltSzam > tippeltSzam){
-                    //TODO: értesítés, hogy a gondolt szám nagyobb
+                    Toast.makeText(MainActivity.this, "A gondolt szám nagyobb", Toast.LENGTH_SHORT).show();
                     eletLevon();
                 } else {
-                    //TODO: VICTORY felugró ablak
+                    builder.setTitle("Győzelem");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
                 }
             }
         });
-        //TODO: Növel és csökkent gombok eseménye
+        btnNovel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tippeltSzam < 10){
+                    tippeltSzam++;
+                    textTippeltSzam.setText(tippeltSzam+"");
+                }else{
+                    Toast.makeText(MainActivity.this, "A szám nem lehet nagyobb mint 10", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnCsokkent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tippeltSzam > 1){
+                    tippeltSzam--;
+                    textTippeltSzam.setText(String.valueOf(tippeltSzam));
+                }else{
+                    Toast.makeText(MainActivity.this, "A szám nem lehet kisebb mint 1", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 
     private void eletLevon() {
@@ -57,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
         }
         elet--;
         if (elet < 1){
-            //TODO: GAME OVER felugró ablak
+            builder.setTitle("Vesztettél").create().show();
         }
     }
 
@@ -70,9 +101,32 @@ public class MainActivity extends AppCompatActivity {
         btnNovel = findViewById(R.id.btn_novel);
         btnCsokkent = findViewById(R.id.btn_csokkent);
         btnTippel = findViewById(R.id.btn_tipp);
+        builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(false).setMessage("Szeretne új játékot játszani?")
+            .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ujJatek();
+                }
+            })
+            .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+    }
+
+    private void ujJatek() {
         tippeltSzam = 1;
         elet = 4;
         gondoltSzam = (int)(Math.random()*10)+1;
+        textTippeltSzam.setText(String.valueOf(tippeltSzam));
+        imageHp1.setImageResource(R.drawable.heart2);
+        imageHp2.setImageResource(R.drawable.heart2);
+        imageHp3.setImageResource(R.drawable.heart2);
+        imageHp4.setImageResource(R.drawable.heart2);
+
     }
 
 
